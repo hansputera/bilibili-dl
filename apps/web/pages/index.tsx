e1e70f6df2d6138d22e7b897096b88e4714a77dc
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-07 07:10:55 +0700

    feat(apps.web): added index.tsx
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,9 @@
+import xwind from 'xwind';
+
+export default function Index() {
+    return (
+        <div css={xwind`text-center`}>
+            <p css={xwind`text-4xl`}>Hello World</p>
+        </div>
+    );
+}
