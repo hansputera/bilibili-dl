f0b8210a8021becc4a46603f88b8fdf25ec2a662
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-07 20:16:38 +0700

    feat(packages.core): completed search function
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ packages/core/src/search.ts
@@ -0,0 +1,44 @@
+import {
+    fetchAPI,
+    ItemTransformed,
+    plainToClass,
+    compare,
+} from '@bilibili-dl/util';
+import {getGatewayURL} from '@bilibili-dl/config/constants.js';
+
+/**
+ * Search videos/anime from given query.
+ * @param {string} query Search query.
+ * @return {Promise<ItemTransformed[]>}
+ */
+// TODO: create pagination request
+export const searchQuery = async (query: string): ItemTransformed[] => {
+    const response = await fetchAPI
+        .get(getGatewayURL('v2').concat('search'), {
+            searchParams: {
+                keyword: encodeURIComponent(q),
+                platform: 'web',
+                s_locale: 'id-ID',
+            },
+        })
+        .json<{
+            data: {
+                items: unknown[];
+            }[];
+        }>();
+
+    return (data = response.data
+        .at(-1)!
+        .items.concat(
+            compare(response.data.at(-1), response.data.at(1))
+                ? []
+                : response.data.at(1)!.items,
+        )
+        .map((t) =>
+            plainToClass(ItemTransformed, t, {
+                strategy: 'excludeAll',
+            }),
+        ));
+};
+
+search('Enen no shouboutai');
