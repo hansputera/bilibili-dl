6c17cd9c6dae684168d7bb5638be4de0606ebe40
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-07 20:44:22 +0700

    feat(apps.web): added search api
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ apps/web/pages/api/search.ts
@@ -0,0 +1,21 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {searchQuery} from '@bilibili-dl/core';
+import * as Validator from 'fastest-validator';
+
+const v = new Validator();
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const validReq = v.compile({
+        query: {
+            type: 'string',
+            min: 5,
+            max: 255,
+        },
+    })(req.body || req.query);
+
+    if (typeof validReq === 'object') {
+        return res.status(400).json(validReq);
+    }
+
+    const result = await searchQuery(req.body.query || req.query.query);
+    return res.status(200).json(result);
+};
