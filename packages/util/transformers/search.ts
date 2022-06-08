e79981a3d23c04b83b7e916878e8d746daa7ecdb
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-06-08 19:43:01 +0700

    fix(packages.util.transformers): fix un-exposed data
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- packages/util/transformers/search.ts
+++ packages/util/transformers/search.ts
@@ -24,15 +24,49 @@ export class ItemTransformed {
     get id(): number {
         return this._videoId || this._seasonId;
     }
-    @Expose({name: 'desc'}) description!: string;
-    @Expose({name: 'cover'}) thumbnail!: string;
-    @Expose({name: 'score'}) rating!: number;
-    @Expose({name: 'styles'}) genre!: string;
+
+    @Expose()
+    @Exclude({toPlainOnly: true})
+    desc!: string;
+
+    @Expose()
+    @Exclude({toPlainOnly: true})
+    cover!: string;
+
+    @Expose()
+    @Exclude({toPlainOnly: true})
+    score!: number;
+
+    @Expose()
+    @Exclude({toPlainOnly: true})
+    styles!: string;
+
+    @Expose()
+    get description(): string {
+        return this.desc;
+    }
+
+    @Expose()
+    get thumbnail(): string {
+        return this.cover;
+    }
+
+    @Expose()
+    get rating(): number {
+        return this.score;
+    }
+
+    @Expose()
+    get genre(): string {
+        return this.styles;
+    }
+
     @Expose({name: 'view'})
     @Transform(
         ({value}) => value.match(/\d+((.|\/)+)?\d+(m|k)?/gi)?.at(0) ?? '0',
     )
     views!: string;
+
     @Expose({name: 'duration'})
     @Transform(({value}) => (value.length ? value : '-'))
     duration!: string;
