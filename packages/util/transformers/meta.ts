f034f3cd4ff49254872de3fd7c7969817e54b268
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-06-10 09:18:04 +0000

    feat(packages.util.transformers): add /api/meta data transformer
    Signed-off-by: GitHub <noreply@github.com>

---
+++ packages/util/transformers/meta.ts
@@ -0,0 +1,21 @@
+// TODO: completing meta data transform.
+export const transformToReadable = (data: any) => {
+    return {
+        title: data.sharedData.title,
+        url: data.sharedData.url,
+        photo: {
+            vertical: data.sharedData.vertical_pic,
+            horizontal: data.sharedData.horizontal_pic,
+        },
+        description: data.sharedData.desc,
+        id: data.OgvVideo.epId || data.UgcVideo.videoData.aid,
+        thumbnail: data.UgcVideo.videoData
+            ? data.UgcVideo.videoData.cover
+            : data.OgvVideo.epDetail.cover,
+        genre: data.OgcVideo.seasonData
+            ? data.OgcVideo.seasonData.styles
+                  .map((style: {title: string}) => style.title)
+                  .join(', ')
+            : undefined,
+    };
+};
