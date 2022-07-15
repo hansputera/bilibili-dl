f121ea2a94edd91e394b0d840f7df120467a1096
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-15 11:14:36 +0000

    fix(packages.util.transformers): fixed unknown sectionsList data
    Signed-off-by: GitHub <noreply@github.com>

--- packages/util/transformers/meta.ts
+++ packages/util/transformers/meta.ts
@@ -97,7 +97,7 @@ export class MetaTransformed {
 
 // TODO: completing meta data transform.
 export const transformMeta = (data: any) => {
-    const sectionsList = data.ogv?.sectionsList?.reduce(
+    const sectionsList = data.ogv?.season?.sectionsList?.reduce(
         (
             prev: {
                 episodes: Record<string, unknown>[];
