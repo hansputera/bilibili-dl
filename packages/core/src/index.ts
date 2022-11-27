9fd2431956c525b1db680386ffc6b9c94c5fd49c
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2022-11-27 13:56:34 +0000

    feat(core): add subtitle
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from './search';
 export * from './playurl';
 export * from './meta';
+export * from './subtitle';
---
+++ packages/core/src/subtitle.ts
@@ -0,0 +1,41 @@
+import {getGatewayURL} from '@bilibili-dl/config/constants.js';
+import type {
+    SubtitleData,
+    SubtitleResponse,
+} from '@bilibili-dl/interfaces/core/subtitle';
+import {fetchAPI, Got} from '@bilibili-dl/util';
+
+export const getSubtitle = async (
+    id: string,
+    locale: string,
+): Promise<SubtitleData | string> => {
+    const response = await fetchAPI(getGatewayURL('v2').concat('subtitle'), {
+        searchParams: {
+            s_locale: 'en_US',
+            platform: 'web',
+            episode_id: id,
+            spm_id: 'bstar-web.pgc-video-detail.0.0',
+            from_spm_id: 'bstar-web.search-result.0.0',
+        },
+        headers: {
+            Origin: 'https://www.bilibili.tv',
+            Referer: 'https://www.bilibili.tv/'.concat(locale),
+        },
+    }).json<SubtitleResponse>();
+
+    if (response.message.length) return response.message;
+    else {
+        const sub = response.data.subtitles.find((s) =>
+            s.lang_key.startsWith(locale),
+        );
+        if (!sub) return "Couldn't find the subtitle";
+
+        const twoResponse = await Got.got
+            .get(sub.url)
+            .json<SubtitleData>()
+            .catch(() => undefined);
+        if (!twoResponse) return "Couldn't get the subtitle contents!";
+
+        return twoResponse;
+    }
+};
