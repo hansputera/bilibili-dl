f444d13c815e89cedfff7632f013e65febc2b889
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2022-11-27 12:52:09 +0000

    feat(interfaces): add Subtitle data
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

--- packages/interfaces/core/subtitle.ts
+++ packages/interfaces/core/subtitle.ts
@@ -1,3 +1,17 @@
+export interface SubtitleData {
+    font_size: number;
+    font_color: string;
+    background_alpha: number;
+    background_color: string;
+    Stroke: string;
+    body: {
+        from: number;
+        to: number;
+        location: number;
+        content: string;
+    }[];
+}
+
 export interface SubtitleItem {
     url: string;
     lang: string;
