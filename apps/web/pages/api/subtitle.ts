61ea44410b859cda13cfc79f2faca94c669013b0
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2022-11-27 14:21:16 +0000

    feat: add subtitle api #2
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

---
+++ apps/web/pages/api/subtitle.ts
@@ -0,0 +1,34 @@
+import Validator from 'fastest-validator';
+import {supportedLocales} from '@bilibili-dl/config/constants.js';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {getSubtitle} from '@bilibili-dl/core';
+import {transformSubtitle} from '@bilibili-dl/util';
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+    const validator = new Validator();
+    const validatorPayload = validator.compile({
+        id: {
+            type: 'string',
+        },
+        locale: {
+            type: 'enum',
+            values: supportedLocales,
+        },
+    })(request.body || request.query);
+
+    if (typeof validatorPayload === 'object') {
+        return response.status(400).json(validatorPayload);
+    }
+
+    const [id, locale] = [
+        (request.body?.id || request.query?.id).toLowerCase(),
+        (request.body?.locale || request.query?.locale).toLowerCase(),
+    ];
+
+    const subres = await getSubtitle(id, locale);
+    if (typeof subres === 'string') {
+        return response.status(500).json({error: subres});
+    }
+
+    return transformSubtitle(subres).pipe(response);
+};
