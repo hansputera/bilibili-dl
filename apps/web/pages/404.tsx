0d5ec1fec9ab8a65c64547dee5205e5175b92c53
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-03-23 08:04:02 +0000

    feat(apps/web): completing custom 404 page
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -1,11 +1,23 @@
+import {useRouter} from 'next/router';
+import NextHead from 'next/head';
+
 /**
  * Custom 404 Page
  * @return {JSX.Element}
  */
-export default function Custom404Page() {
+export default function Custom404Page(): JSX.Element {
+    const router = useRouter();
     return (
         <>
-            <pre>{'{ Oops! }'}</pre>
+            <NextHead>
+                <title>404 Not Found</title>
+            </NextHead>
+            <h1 className="text-2xl text-center">
+                Oops! Try{' '}
+                <button className="text-blue-400" onClick={router.back}>
+                    back
+                </button>
+            </h1>
         </>
     );
 }
