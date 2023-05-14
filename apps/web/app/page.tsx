18a5e4d55c72a86a0de14971647b0f5e009f714a
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-05-15 06:47:42 +0700

    chore: update configuration for debugging and monitoring

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 /**
  * Index page.
  * @return {JSX.Element}
@@ -6,6 +8,14 @@ export default function Index() {
     return (
         <div className="container">
             <p className="text-4xl">Hello world</p>
+            <button
+                onClick={() => {
+                    throw new Error('Frontend Error!');
+                }}
+            >
+                Test Sentry | Break the world
+            </button>
+            ;
         </div>
     );
 }
--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -5,4 +5,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
     dsn: SENTRY_DSN,
     tracesSampleRate: 1.0,
+    integrations: [new Sentry.Replay()],
 });
