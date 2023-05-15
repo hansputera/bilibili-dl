d701a115df055a18b0338e4ca9bb7e0ba089cdf4
Reyuki-san <hikikomori.yukisan@gmail.com>
2023-05-15 08:37:10 +0700

    chore: add tailwind prettier plugin

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,14 @@
-'use client';
+import Navbar from "../components/Navbar";
 
 /**
  * Index page.
  * @return {JSX.Element}
  */
 export default function Index() {
-    return (
-        <div className="container">
-            <p className="text-4xl">Hello world</p>
-            <button
-                onClick={() => {
-                    throw new Error('Frontend Error!');
-                }}
-            >
-                Test Sentry | Break the world
-            </button>
-            ;
-        </div>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto"></div>
+    </>
+  );
 }
---
+++ apps/web/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="w-full p-4">
+      <h1 className="text-xl">Bilibili Downloader</h1>
+    </div>
+  );
+}
